80c6f467fe61f82219acdb6dd04645745e81fbf7
kpreisser <kpreisser@users.noreply.github.com>
2023-09-09 22:42:31 +0200

    Use quotes for the application path, to avoid an ambiguity when it contains spaces.
    See the following link for how a path with spaces but without quotes would be interpreted in such a case: https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createprocessw#parameters

--- TcpTunnel/InstallService.cmd
+++ TcpTunnel/InstallService.cmd
@@ -1,4 +1,4 @@
 set "ServiceName=TcpTunnel"
-sc create "%ServiceName%" binpath= "%~dp0TcpTunnel.exe -service" start=delayed-auto
+sc create "%ServiceName%" binpath= "\"%~dp0TcpTunnel.exe\" -service" start=delayed-auto
 sc failure "%ServiceName%" reset=0 actions=restart/60000/restart/60000/restart/60000
 @pause
