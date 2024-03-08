e51e47b797de6f99580d9dab66217b9fa141dd70
kpreisser <kpreisser@users.noreply.github.com>
2024-03-08 15:11:27 +0100

    Remove unused fields/properties.

--- TcpTunnel/Networking/ReceivedMessage.cs
+++ TcpTunnel/Networking/ReceivedMessage.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 
 namespace TcpTunnel.Networking;
 
@@ -9,16 +8,10 @@ internal struct ReceivedMessage
 
     private readonly ReceivedMessageType type;
 
-    private byte[]? convertedBytes;
-
-    private string? convertedString;
-
     public ReceivedMessage(Memory<byte> buffer, ReceivedMessageType type)
     {
         this.buffer = buffer;
         this.type = type;
-        this.convertedBytes = null;
-        this.convertedString = null;
     }
 
     public ReceivedMessageType Type
@@ -36,15 +29,5 @@ internal struct ReceivedMessage
     public Memory<byte> Buffer
     {
         get => this.buffer;
-    }
-
-    public byte[] ByteMessage
-    {
-        get => this.convertedBytes ??= this.buffer.ToArray();
-    }
-
-    public string StringMessage
-    {
-        get => this.convertedString ??= Encoding.UTF8.GetString(this.buffer.Span);
     }
 }
